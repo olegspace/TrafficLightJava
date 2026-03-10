1077694ee08a85d6594c325f67a16ac41249c280
olegspace <olezhka_revin@mail.ru>
2026-03-10 12:57:26 +0700

    Final commit

--- app/src/main/java/com/example/trafficlightjava/MainActivity.java
+++ app/src/main/java/com/example/trafficlightjava/MainActivity.java
@@ -1,24 +1,50 @@
 package com.example.trafficlightjava;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ConstraintLayout mConstraintLayout;
+    private TextView mInfoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        mConstraintLayout = findViewById(R.id.root_layout);
+        mInfoTextView = findViewById(R.id.textView);
+
+        // Назначаем этот класс обработчиком для всех трёх кнопок
+        Button buttonRed = findViewById(R.id.red_button);
+        Button buttonYellow = findViewById(R.id.yellow_button);
+        Button buttonGreen = findViewById(R.id.green_button);
+
+        buttonRed.setOnClickListener(this);
+        buttonYellow.setOnClickListener(this);
+        buttonGreen.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.red_button) {
+            mInfoTextView.setText(R.string.red);
+            mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.redColor));
+        } else if (id == R.id.yellow_button) {
+            mInfoTextView.setText(R.string.yellow);
+            mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.yellowColor));
+        } else if (id == R.id.green_button) {
+            mInfoTextView.setText(R.string.green);
+            mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.greenColor));
+        }
     }
 }
